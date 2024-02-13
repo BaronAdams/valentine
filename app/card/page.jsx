342f9f5cee57dfe95b7713c1e5bb1b454588bb64
BaronAdams <rogeradamskouyangtaptoussia@gmail.com>
2024-02-13 05:31:20 +0100

    Last updates

--- app/card/page.jsx
+++ app/card/page.jsx
@@ -41,26 +41,26 @@ function Card() {
   if(!username || !valentine_name || !valentine_img || !message) router.push('/')
 
   return (
-      <main className="flex flex-col w-screen min-h-screen items-center overflow-x-hidden">
-        <div className="w-[25%] h-[55vh] relative overflow-hidden max-[900px]:h-[65vh] max-[900px]:w-[50%] max-[700px]:w-[75%] max-[700px]:h-[70vh]  max-[400px]:w-full" style={{backgroundImage:`url('${image}')`, backgroundPosition:"center", backgroundSize:"cover"}}>
+      <main className="flex max-[900px]:flex-col w-screen min-h-screen items-center overflow-x-hidden">
+        <div className="min-[900px]:w-[55%] h-[55vh] min-[900px]:h-screen relative overflow-hidden max-[900px]:h-[65vh] max-[900px]:w-full max-[700px]:h-[70vh]" style={{backgroundImage:`url('${image}')`, backgroundPosition:"center", backgroundSize:"cover"}}>
           {/* <Image width={40} height={40} src={"/images/reginia.jpg"} className="absolute z-1 w-full h-full object-cover" alt="Valentine image" /> */}
           <Image width={40} height={40} src={"/images/13ea8843c050622e2055b313c2392aba.webp"} className="absolute z-20 w-full h-full object-cover" alt="Valentine image" />
           <Image width={40} height={40} src={"/images/icegif-4148.gif"} className="absolute z-20 w-full h-full object-cover" alt="Valentine image" />
           <Image width={40} height={40} src={"/images/ca5aee534774d6b4ef69168c1c75a9d1.gif"} className="absolute z-20 w-full h-full object-cover" style={{transform:'scale(0.8)'}} alt="Valentine image" />
           <Image width={40} height={40} src={"/images/b31896187af7300a0471cd111b0ab7d0.webp"} className="absolute z-20 w-full h-full object-cover" alt="Valentine image" />
-          <img width={40} height={40} src={"/images/cd21674affb214f521de370a68c6582d.png"} className="absolute z-20 w-full h-full object-cover mt-[180px] " alt="Valentine image" />
+          <img width={40} height={40} src={"/images/cd21674affb214f521de370a68c6582d.png"} className="absolute z-20 w-full h-full object-cover mt-[180px] min-[900px]:mt-[250px] " alt="Valentine image" />
           <div className="absolute bg-[#00000062] w-full h-full z-10"></div>
           <div className="absolute w-full h-full z-30 flex justify-center items-center">
             <p className={`${lovaQuarrel.className} text-white text-[65px]`}>For you {valentine_name}</p>
           </div>
         </div>
-        <div className={`w-full min-h-[300px] bg-[#ec9ca9] flex flex-col items-center gap-6 text-[30px] leading-tight pt-4 p-3 overflow-hidden`}>
+        <div className={`w-full min-[900px]:w-[45%] min-h-[300px] min-[900px]:h-screen bg-[#ec9ca9] flex flex-col items-center min-[900px]:justify-center gap-6 text-[30px] leading-tight pt-4 p-3 overflow-hidden`}>
           <div className={`${belleza.className} text-justify w-[88%] `}>
             <p className={`mb-4 text-[20.4px]`}>Happy <span className={`${brittany.className} text-[25.6px]`}>Valentine</span> my love <img width={46} height={46} className="inline object-cover" src="/images/hearts.png" alt="HEARTS" /></p>
             <p className={`${raleway.className} w-full text-[17.8px]`}>{finalMessage}</p>
             <p className={`${brittany.className} w-full mt-3 text-end text-[17.8px]`}>{username}</p>
           </div> 
-          <div className="w-[42%] h-[170px] relative">
+          <div className="w-[42%] min-[634px]:w-[35%] h-[170px] min-[700px]:h-[200px] min-[830px]:w-[32%] min-[830px]:h-[230px] relative">
             <div className="w-full h-full absolute z-10 rotate-[-15.6deg] flex justify-center items-center bg-white">
               <img src={`${image}`} className="w-[90%] h-[90%] object-cover" />
             </div>
--- app/page.js
+++ app/page.js
@@ -20,7 +20,10 @@ export default function Home() {
   const [file, setFile] = useState();
   const [firstName, setfirstName] = useState();
   const [secondName, setsecondName] = useState();
+  const [msg, setMsg] = useState();
   const [loading, setloading] = useState(false);
+  const [isSend, setisSend] = useState(false);
+  const [finished, setIsfinished] = useState(false);
 
   const handleChangeFile = (e)=>{
     setFile(e.target?.files[0])
@@ -112,6 +115,7 @@ export default function Home() {
         downloadLink.download = `${elt.valentine_name}.png`;
         downloadLink.click();
         document.body.removeChild(tmp);
+        setIsfinished(true);
       });
   }
 
@@ -119,69 +123,75 @@ export default function Home() {
     setloading(true)
   }
   
-
   useEffect(()=>{
     console.log(state)
     if(state?.url || state?.username || state?.file || state?.valentine_name || state?.error){
       setloading(false)
     } 
-    if(state?.user?.username) generateImg(state.user)
+    if(state?.user?.username) {
+      setisSend(true)
+      generateImg(state.user)
+    }
   },[state])
 
   return (
     <>
-    <nav className="bg-white overflow-hidden dark:bg-gray-900 fixed w-full z-20 flex flex-wrap items-center justify-center h-[9vh] top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-      <Link href="/" className="flex h-full w-full items-center space-x-3 rtl:space-x-reverse">
-          <img width={40} height={40} src="/logo.jpg" className="h-full w-full object-cover object-center" style={{transform:'scale(0.65)'}} alt="Valentine's Logo"/>
+    <nav className="bg-white overflow-hidden fixed w-full z-20 flex flex-wrap items-center justify-center h-[9vh] top-0 start-0 border-b border-gray-200">
+      <Link href="/" className="flex h-full w-full justify-center items-center space-x-3 rtl:space-x-reverse">
+          <img src="/logo.jpg" className="h-full w-full object-cover object-center scale-[0.65] min-[530px]:w-[80%] min-[610px]:w-[70%] min-[730px]:w-[60%] min-[865px]:w-[54%] min-[880px]:w-[50%] min-[1000px]:w-[46%] min-[1050px]:w-[42%] min-[1100px]:w-[40%]  " alt="Valentine's Logo"/>
           {/* <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span> */}
       </Link>
     </nav>
     <form onSubmit={handleSubmit} action={dispatch} className="flex flex-col mt-[9vh] pt-3 w-screen min-h-screen gap-5 justify-center items-center">    
-      <div className="flex flex-col gap-3 items-center justify-center w-[80%]">
-          <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+      <div className="flex flex-col gap-3 items-center justify-center w-[32%] max-[640px]:w-[60%] max-[960px]:w-[50%] max-[480px]:w-[80%]">
+          <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
               <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                  <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                  <svg className="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                       <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
                   </svg>
-                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400 text-center"><span className="font-semibold">Click to upload your valentine picture</span>or drag and drop</p>
-                  <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                  <p className="mb-2 text-sm text-gray-500 text-center"><span className="font-semibold">Click to upload your valentine picture</span>or drag and drop</p>
+                  <p className="text-xs text-gray-500">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
               </div>
               <input onChange={handleChangeFile} id="dropzone-file" name="image" type="file" className="hidden" accept="image/*" required />
           </label>
-          {((state && !file) || (state?.file && !file)) && <p className="mt-2 text-sm text-red-600 dark:text-red-500"><span className="font-medium">{state?.file}</span></p>}
+          {((state && !file) || (state?.file && !file)) && <p className="mt-2 text-sm text-red-600"><span className="font-medium">{state?.file}</span></p>}
       </div>
       {file && <img
         width={30}
         height={30}
         alt={"Valentine's image"}
         src={URL.createObjectURL(file)}
-        className="w-[80%] h-[40vh] object-cover object-center"
+        className="w-[32%] max-[640px]:w-[60%] max-[960px]:w-[50%] max-[480px]:w-[80%] h-[40vh] object-cover object-center"
       /> }
-      <div className="w-[50%] max-[480px]:w-[80%]">
+      <div className="w-[32%] max-[640px]:w-[60%] max-[960px]:w-[50%] max-[480px]:w-[80%]">
         <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
-        <input onChange={(e)=>setfirstName(e.target.value)} type="text" id="username" name="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required placeholder="Tap your first name" />
+        <input onChange={(e)=>setfirstName(e.target.value)} type="text" id="username" name="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required placeholder="Tap your first name" />
         {((state && !firstName) || (state?.username && !firstName)) && <p className="mt-2 text-sm text-red-600 dark:text-red-500"><span className="font-medium">{state?.username}</span></p>}
       </div>  
-      <div className="w-[50%] max-[480px]:w-[80%]">
+      <div className="w-[32%] max-[640px]:w-[60%] max-[960px]:w-[50%] max-[480px]:w-[80%]">
         <label htmlFor="valentine_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your valentine&apos;s name</label>
-        <input onChange={(e)=>setsecondName(e.target.value)} type="text" id="valentine_name" name="valentine_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required placeholder="Tap your valentine's name" />
+        <input onChange={(e)=>setsecondName(e.target.value)} type="text" id="valentine_name" name="valentine_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required placeholder="Tap your valentine's name" />
         {((state && !secondName) || (state?.valentine_name && !secondName)) && <p className="mt-2 text-sm text-red-600 dark:text-red-500"><span className="font-medium">{state?.valentine_name}</span></p>}
       </div>  
-      <div className="w-[50%] max-[480px]:w-[80%]">
-        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message for him/her(optionnal)</label>
-        <textarea name="message" minLength={20} id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your wishes here..."></textarea>
+      <div className="w-[32%] max-[640px]:w-[60%] max-[960px]:w-[50%] max-[480px]:w-[80%]">
+        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 ">Your message for him/her(optionnal)</label>
+        <textarea onChange={(e)=>setMsg(e.target.value)} name="message" minLength={20} maxLength={50} id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 " placeholder="Write your wishes here..."></textarea>
       </div>
       <div className="w-[100%] flex flex-col items-center">
-        <button type="submit" disabled={loading} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button type="submit" disabled={loading || isSend} className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center ${isSend && 'bg-green-600 hover:bg-green-600'}`}>
           {loading ? (<div><svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
           <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
-          </svg> Loading...</div>) : 'Send'}
+          </svg> Loading...</div>) : isSend ? (<div><svg className="inline w-4 h-4 me-3 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z" fill="white"></path></svg> Done</div>) : 'Send'}
         </button>
         {state?.error && <p className="mt-2 text-sm text-red-600 dark:text-red-500"><span className="font-medium">{state.error}</span></p>}
       </div>
   </form> 
-  {state?.url &&  <div className="w-[100%] mt-2 flex flex-col items-center gap-2">
+  {state?.url && !finished && <div className="w-[100%] mt-2 flex justify-center items-center">
+  <svg className="inline w-[40px] h-[40px] me-2 text-blue-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><circle cx={18} cy={12} r={0} fill="currentColor"><animate attributeName="r" begin={0.67} calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"></animate></circle><circle cx={12} cy={12} r={0} fill="currentColor"><animate attributeName="r" begin={0.33} calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"></animate></circle><circle cx={6} cy={12} r={0} fill="currentColor"><animate attributeName="r" begin={0} calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"></animate></circle></svg>
+    <span className="inline">Generating image and links</span>
+  </div>}
+  {state?.url && finished &&  <div className="w-[100%] mt-2 flex flex-col items-center gap-2">
     <a className="block font-medium text-blue-600 underline " href={`${state.url}`}>Tap this link &#128513; </a> 
     <p className="font-bold">Or</p>
     <a className="block font-medium" href={`whatsapp://send?text=${encodeURIComponent(`${firstName} has a gift for ${secondName} \u{1FA77}\u{1F381}\u{1F970} :\n${state.url}\n\n\nTry your own there \u{1F447} :\n${state.baseUrl}`)}`} data-action="share/whatsapp/share">Share to your valentine in WhatsApp <img width={25} height={25} src="/images/whatsapp.png" className="object-cover object-center inline" alt="Whatsapp icon"/></a> </div>}
